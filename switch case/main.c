676720a7ce8fbf071eda447d332ba7d69808db1e
xalo <xalo@xalodeMac-mini-9.local>
2015-07-27 18:01:16 +0800

    Initial Commit

---
+++ switch case/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  switch case
+//
+//  Created by xalo on 15/7/27.
+//  Copyright (c) 2015年 xalo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
