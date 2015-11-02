d7d3334a41efca7e15599a02a4f43465c2fa7fd7
VicLi <351105136@qq.com>
2015-11-02 22:48:13 +0800

    SVProgressHUD - VicLi
    SVProgressHUD

--- switch case/main.c
+++ switch case/main.c
@@ -8,8 +8,41 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
-}
+
+
+    int main(int argc, const char * argv[]) {
+        //    int a = (arc4random()%41+20);
+        int a = 0;
+        printf("请输入一个数字(1-7)");
+        scanf("%d",&a);
+        switch (a) {
+            case 1:
+                printf("Monday\n");
+                break;
+            case 2:
+                printf("\nTuesday");
+                break;
+            case 3:
+                printf("\nWedensday");
+                break;
+            case 4:
+                printf("\nThursday");
+                break;
+            case 5:
+                printf("Friday\n");
+                break;
+            case 6:
+                printf("Satuiday\n");
+                break;
+            case 7:
+                printf("\nSunday");
+                break;
+                
+            default:
+                break;
+        }
+        
+        
+    }
+
+
